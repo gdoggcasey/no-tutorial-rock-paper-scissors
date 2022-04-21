495673023ae98c211f06602aedbcfd5ddb6fa90b
Casey <gdoggcaseysc2@gmail.com>
2022-04-21 11:47:09 -0400

    Struggled in to implement playROund function. Finally got it working for 1 round.

--- script.js
+++ script.js
@@ -5,7 +5,7 @@
 // We compare the two options and select a winner
 
 const choices = ['rock', 'paper', 'scissors'];
-const results = playRound();
+
 
 
 function computerPlay() {
@@ -15,17 +15,33 @@ function computerPlay() {
 
 function userPlay() {
     userSelection = prompt('Please choose either Rock, Paper, or Scissors.')
+    userSelection.toLowerCase();
     return userSelection;
 }
 
-function playRound(computerSelection, userSelection) {
+function playRound() {
     computerPlay();
     userPlay();
-    return results;
+    if (userSelection === 'rock' && computerSelection === 'scissors') {
+        console.log('User: Rock - CPU: Scissors --- User wins!');
+    } else if (userSelection === 'rock' && computerSelection === 'paper') {
+        console.log('User: Rock - CPU: Paper --- SPU wins!');
+    } else if (userSelection === 'rock' && computerSelection === 'rock') {
+        console.log('User: Rock - CPU: Rock --- Tie game!');
+    } else if (userSelection === 'paper' && computerSelection === 'rock') {
+        console.log('User: Paper - CPU: Rock --- User wins!');
+    } else if (userSelection === 'paper' && computerSelection === 'scissors') {
+        console.log('User: Paper - CPU: Scissors --- CPU wins!');
+    } else if (userSelection === 'paper' && computerSelection === 'paper') {
+        console.log('User: Paper - CPU: Paper --- Tie game!');
+    } else if (userSelection === 'scissors' && computerSelection === 'paper') {
+        console.log('User: Scissors - CPU: Paper --- User wins!');
+    } else if (userSelection === 'scissors' && computerSelection === 'rock') {
+        console.log('User: Scissors - CPU: Rock --- CPU wins!');
+    } else if (userSelection === 'scissors' && computerSelection === 'scissors') {
+        console.log('User: Scissors - CPU: Scissors --- Tie game!');
+    } 
 }
+    
 
-
-
-
-//console.log(playRound());
-
+playRound();
